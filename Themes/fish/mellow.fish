fb6e6f43361d3b757ce96a5cb8fee44a57de1742
Maleonams <amselemmatteo2@gmail.com>
2025-11-14 21:04:31 +0100

    We got a new branch!: Themes (i'll put everything other than qt and hypr)

---
+++ Themes/fish/mellow.fish
@@ -0,0 +1,58 @@
+# 888'Y88 'e'       888
+# 888 ,'Y      dP"Y 888 ee
+# 888C8   888 C88b  888 88b
+# 888 "   888  Y88D 888 888
+# 888     888 d,dP  888 888
+
+
+#     e   e             888 888
+#    d8b d8b     ,e e,  888 888  e88 88e  Y8b Y8b Y888P
+#   e Y8b Y8b   d88 88b 888 888 d888 888b  Y8b Y8b Y8P
+#  d8b Y8b Y8b  888   , 888 888 Y888 888P   Y8b Y8b "
+# d888b Y8b Y8b  "YeeP" 888 888  "88 88"     YP  Y8P
+
+# NOTE: put it in ~/.config/fish/conf.d/mellow.fish  pwease :3
+
+# --- Base ---
+set -U fish_color_normal '#eaddcf'
+set -U fish_color_command '#ffe3a9'
+set -U fish_color_param '#c3afff'
+set -U fish_color_quote '#a67ee9'
+set -U fish_color_redirection '#5f4d8c'
+set -U fish_color_operator '#b093ff'
+set -U fish_color_end '#b093ff'
+set -U fish_color_comment '#8c7acd'
+set -U fish_color_autosuggestion '#513a87'
+set -U fish_color_valid_path '#c3afff'
+
+# --- Errors / Warnings ---
+set -U fish_color_error '#5b31c7'
+set -U fish_color_cancel '#39149c'
+set -U fish_color_match '#fff2c7'
+
+# --- Prompt & Directory ---
+set -U fish_color_cwd '#ffe3a9'
+set -U fish_color_cwd_root '#ffb6b6'
+set -U fish_color_user '#ffe3a9'
+set -U fish_color_host '#a287ff'
+
+# --- Search / Pager / Selection ---
+set -U fish_color_search_match '#fff0ca' --background='#5f4d8c'
+set -U fish_color_selection '#1a1325' --background='#c3afff'
+set -U fish_pager_color_prefix '#ffe3a9'
+set -U fish_pager_color_completion '#eaddcf'
+set -U fish_pager_color_description '#b093ff'
+set -U fish_pager_color_progress '#39149c'
+
+# --- Misc ---
+set -U fish_color_history_current '#b093ff'
+set -U fish_color_status '#ffb6b6'
+set -U fish_color_escape '#ffe3a9'
+set -U fish_color_cancel '#5b31c7'
+set -U fish_color_operator '#b093ff'
+set -U fish_color_option '#c3afff'
+
+# --- Cursor / Background / Foreground (for integration with kitty :3) ---
+set -U fish_color_foreground '#eaddcf'
+set -U fish_color_background '#1a1325'
+set -U fish_color_cursor '#ffe3a9'
