57895ade7503e8325645183ba46eaa35b8fa2352
Maleonams <amselemmatteo2@gmail.com>
2026-01-24 01:24:15 +0100

    smol updt for optimisation and looks V1.2

--- waybar/scripts/ddc_daemon.py
+++ waybar/scripts/ddc_daemon.py
@@ -32,7 +32,7 @@ def worker():
             elapsed = time.time() - last_change
             value = brightness
         # applique SEULEMENT si stable depuis 200 ms
-        if elapsed > 2:
+        if elapsed > 1:
             subprocess.Popen(
                 ["ddcutil", "-b", BUS, "setvcp", "10", str(value)],
                 stdout=subprocess.DEVNULL,
